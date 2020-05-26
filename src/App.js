41c904de334c3408bb10fc06e284dc01089b3fd5
unknown <ambady@safecity.com>
2020-05-26 15:13:54 +0400

    added router modules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
-
 import HomePage from './components/homepage/homepage.component'
+
+const HastsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
-    <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={HastsPage} />
+      </Switch>
+    </div>
   );
 }
 
