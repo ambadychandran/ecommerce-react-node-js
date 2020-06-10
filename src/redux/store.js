e20517cfae9c570e8848f54660a035c19afbecc6
unknown <ambady@safecity.com>
2020-06-10 19:20:21 +0400

    logger removed for production

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,10 @@ import { persistStore } from 'redux-persist';
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
+
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 const persistor = persistStore(store);
 export { store, persistor };
